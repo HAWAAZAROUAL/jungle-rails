7adf0644cc46b543ce2c186d291a3b152f8b6489
Hawaa <hawaazaroual@gmail.com>
2021-09-21 11:40:15 -0600

    add first successful validate

--- spec/models/product_spec.rb
+++ spec/models/product_spec.rb
@@ -4,7 +4,13 @@ RSpec.describe Product, type: :model do
   pending "add some examples to (or delete) #{__FILE__}"
   RSpec.describe Product, type: :model do
     describe 'Validations' do
-      # validation tests/examples here
+      # when everything is present and saves
+      it "saves when all fields are filled" do
+        @category = Category.new(name: 'test category')
+        @product = @category.products.new(name: 'test product', price: 30, quantity: 3)
+        @product.save!
+      
+
     end
   end
 end
