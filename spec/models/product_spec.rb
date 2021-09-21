cf60bbf495fbc1d1ef3f4fe107ca0e9cde378b1f
Hawaa <hawaazaroual@gmail.com>
2021-09-21 10:26:51 -0600

    create product rspec

---
+++ spec/models/product_spec.rb
@@ -0,0 +1,10 @@
+require 'rails_helper'
+
+RSpec.describe Product, type: :model do
+  pending "add some examples to (or delete) #{__FILE__}"
+  RSpec.describe Product, type: :model do
+    describe 'Validations' do
+      # validation tests/examples here
+    end
+  end
+end
