bfcab90cf4669f420e5dacb912ca19978b592f6e
Hawaa <hawaazaroual@gmail.com>
2021-09-20 20:20:27 -0600

    add signup form

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,5 +4,19 @@ class UsersController < ApplicationController
   end
 
   def create
+
+    user = User.new(user_paramd)
+    if user.save
+      session[:user_id] = user.id
+      redirect_to '/'
+    else
+      redirect_to '/signup'
+    end
   end
+
+  private
+  def user_params
+    params.require(:user).permit(:password, :password_confirmation, :name, :email)
+  end
+
 end
