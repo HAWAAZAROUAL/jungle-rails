bdc9758245ccb51f833988f4c9eded308f10ada4
Hawaa <hawaazaroual@gmail.com>
2021-09-21 15:48:46 -0600

    finish home-page testing

--- spec/features/home_page_spec.rb
+++ spec/features/home_page_spec.rb
@@ -5,6 +5,14 @@ RSpec.feature "Visitor navigates to home page", type: :feature, js: true do
   scenario "They see all products" do
     # ACT
     expect(page).to have_css 'article.product'
+
+    @category.products.create!(
+  name:  Faker::Hipster.sentence(3),
+  description: Faker::Hipster.paragraph(4),
+  image: open_asset('apparel1.jpg'),
+  quantity: 10,
+  price: 64.99
+)
     visit root_path
 
     # DEBUG / VERIFY
